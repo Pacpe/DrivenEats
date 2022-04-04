75750e79c6caf90971821ff027de26fe50da71dd
Pachelli <pachelli_moraes@hotmail.com>
2022-04-04 18:48:03 -0300

    Conclusao do projeto

--- scripts.js
+++ scripts.js
@@ -7,29 +7,31 @@ let alimento3;
 let dinheiro;
 let dinheiro2;
 let dinheiro3;
+let back;
 
 
 
 function pratosborda(elemento, food, money) {
     prt = elemento.innerHTML;
     
-    const selecionar = document.querySelector(".pratos").querySelector(".selecionado");
+    let selecionar = document.querySelector(".pratos").querySelector(".selecionado");
 
     if (selecionar !== null) {
-        selecionar.classList.remove("selecionado");        
+        selecionar.classList.remove("selecionado"); 
     }
 
     elemento.classList.add("selecionado");
+
     
     alimento = food;
-    dinheiro = money;    
-    
+    dinheiro = money;
 
     if (prt && bbd && sobr) {
         document.querySelector(".esperadeselecao").classList.add("verde");
-    }    
+        document.querySelector(".botaofinal").innerHTML = (`Fechar pedido`);
+        let botao = document.querySelector(".botaofinal").classList.add("aumentarMargem");        
+    }        
 }
-
 
 function bebidasborda(elemento2, food2, money2) {
     bbd = elemento2.innerHTML;
@@ -47,6 +49,8 @@ function bebidasborda(elemento2, food2, money2) {
 
     if (prt && bbd && sobr) {
         document.querySelector(".esperadeselecao").classList.add("verde");
+        document.querySelector(".botaofinal").innerHTML = (`Fechar pedido`);  
+        let botao = document.querySelector(".botaofinal").classList.add("aumentarMargem");      
     }
 }
 
@@ -66,20 +70,34 @@ function sobremesasborda(elemento3, food3, money3) {
 
     if (prt && bbd && sobr) {
         document.querySelector(".esperadeselecao").classList.add("verde");
-        document.querySelector(".botaofinal").innerHTML = `Fechar pedido`;
-        
+        document.querySelector(".botaofinal").innerHTML = (`Fechar pedido`);
+        let botao = document.querySelector(".botaofinal").classList.add("aumentarMargem");        
     }    
 }
 
 function whatsapp() {
-    let ValorTotal = dinheiro + dinheiro2 + dinheiro3;    
-    let texto = (`Olá, gostaria de fazer o pedido:
-    - Prato: ${alimento}
-    - Bebida: ${alimento2}
-    - Sobremesa: ${alimento3}
-    Total: R$ ${ValorTotal.toFixed(2)}`);   
-    let frase = encodeURIComponent(texto);
-    window.location.href = (`https://wa.me/?text=${frase}`);    
+
+    if (prt && bbd && sobr) {
+        let ValorTotal = dinheiro + dinheiro2 + dinheiro3; 
+
+        let nome = prompt("Qual seu nome");
+        let endereco = prompt("Qual seu endereco");
+        let texto = (`Olá, gostaria de fazer o pedido:
+        - Prato: ${alimento}
+        - Bebida: ${alimento2}
+        - Sobremesa: ${alimento3}
+        Total: R$ ${ValorTotal.toFixed(2)}
+
+        Nome: ${nome}
+        Endereço: ${endereco}`);  
+         
+        let frase = encodeURIComponent(texto);
+        window.location.href = (`https://wa.me/?text=${frase}`);    
+    }    
+    
 }
 
 
+ 
+
+     
